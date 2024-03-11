671d6890280059a2cb118ea12af907e807421b6e
amicusdeveloper89 <amicusdeveloper89@gmail.com>
2024-03-11 14:04:27 +0900

    add clickListener

--- app/src/main/java/com/jctech2024/githubtest/activities/MainActivity.kt
+++ app/src/main/java/com/jctech2024/githubtest/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jctech2024.githubtest.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.jctech2024.githubtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView( binding.root )
+        
+        binding.tv.setOnClickListener { Toast.makeText(this, "clicked textview", Toast.LENGTH_SHORT).show() }
     }
 }
